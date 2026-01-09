94bae60ae46c92a0b8d43f5e5adfac8356fdbd50
koh-tom <223271102+koh-tom@users.noreply.github.com>
2026-01-09 10:49:27 +0900

    feat: omarchy用初期設定

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- 設定変更を自動リロード
+config.automatically_reload_config = true
+
+-- フォント設定
+config.font_size = 12.0
+config.font = wezterm.font("JetBrains Mono")
+
+-- hyprlad環境
+config.enable_wayland = false
+
+-- 透明度
+config.window_background_opacity = 0.5
+
+-- ステータスバー更新
+config.status_update_interval = 1000
+
+-- 各モジュールの読み込み
+
+return config
